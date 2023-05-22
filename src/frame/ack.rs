3a0083f87e17e244d9479b94db46ea6c6480d119
Gscienty <gaoxiaochuan@hotmail.com>
2023-05-23 02:55:49 +0800

    impl ack getter / setter

--- src/frame/ack.rs
+++ src/frame/ack.rs
@@ -1,4 +1,4 @@
-use crate::util;
+use crate::{attr::PacketNumber, util};
 
 use super::{
     serialize::{Deserializer, Serializer},
@@ -35,11 +35,21 @@ use super::{
 ///     ACK Range (..) ...,
 ///     [ECN Counts (...)]
 /// }
-pub struct ACKFrame {
-    largest: u64,
+pub(crate) struct ACKFrame {
+    /// 对方确认的最大数据包编号
+    /// 通常这是对等方在生成ACK帧之前接收到的最大数据包编号
+    largest: PacketNumber,
+
+    /// 以微妙为单位的延迟
     delay: u64,
-    first_range: u64,
+
+    /// 正在确认的最大确认数据包之前的连续数据包数量
+    first_range: usize,
+
+    /// 交替出现未被确认的范围（Gap）和被确认的范围（Ack Range）
     ranges: Vec<ACKRange>,
+
+    /// ECN Counts
     ecn: Option<ECNCounts>,
 }
 
@@ -57,9 +67,9 @@ pub struct ACKFrame {
 ///     ACK Range Length (i),
 /// }
 #[derive(Clone, Copy)]
-pub struct ACKRange {
-    gap: u64,
-    length: u64,
+pub(crate) struct ACKRange {
+    gap: usize,
+    length: usize,
 }
 
 /// ECN
@@ -73,20 +83,26 @@ pub struct ACKRange {
 ///     ECN-CE Count (i),
 /// }
 #[derive(Clone, Copy)]
-pub struct ECNCounts {
+pub(crate) struct ECNCounts {
     ect0: u64,
     ect1: u64,
     ecn_ce: u64,
 }
 
 impl ACKFrame {
-    pub fn new(with_ecm: bool) -> Self {
+    /// 构造一个 ACK 帧
+    ///
+    /// # Arguments
+    /// `with_ecn` - 是否携带 ECN
+    /// # Returns
+    /// 返回一个 ACK 帧
+    pub(crate) fn new(with_ecn: bool) -> Self {
         Self {
             largest: 0,
             delay: 0,
             first_range: 0,
             ranges: Vec::new(),
-            ecn: if with_ecm {
+            ecn: if with_ecn {
                 Some(ECNCounts {
                     ect0: 0,
                     ect1: 0,
@@ -96,6 +112,96 @@ impl ACKFrame {
                 None
             },
         }
+    }
+
+    /// 获取最大的确认数据包编号
+    ///
+    /// # Returns
+    /// 返回最大的确认数据包编号
+    #[inline(always)]
+    pub(crate) const fn get_largest(&self) -> PacketNumber {
+        self.largest
+    }
+
+    /// 设置最大的确认数据包编号
+    ///
+    /// # Arguments
+    /// `largest` - 最大的确认数据包编号
+    #[inline(always)]
+    pub(crate) fn set_largest(&mut self, largest: PacketNumber) {
+        self.largest = largest
+    }
+
+    /// 获取延迟
+    ///
+    /// # Returns
+    /// 返回延迟时间
+    #[inline(always)]
+    pub(crate) const fn get_delay(&self) -> u64 {
+        self.delay
+    }
+
+    /// 设置延迟
+    ///
+    /// # Arguments
+    /// `delay` - 延迟时间
+    #[inline(always)]
+    pub(crate) fn set_delay(&mut self, delay: u64) {
+        self.delay = delay
+    }
+
+    /// 获取最大确认数据包之前连续数据包数量
+    ///
+    /// # Returns
+    /// 返回数据包数量
+    #[inline(always)]
+    pub(crate) const fn get_first_range(&self) -> usize {
+        self.first_range
+    }
+
+    /// 设置最大确认数据包之前连续数据包数量
+    ///
+    /// # Arguments
+    /// `first_range` - 连续数据包数量
+    #[inline(always)]
+    pub(crate) fn set_first_range(&mut self, first_range: usize) {
+        self.first_range = first_range
+    }
+
+    /// 获取 ECN Counts
+    ///
+    /// # Returns
+    /// 返回 ECN Counts
+    #[inline(always)]
+    pub(crate) const fn get_ecn(&self) -> Option<ECNCounts> {
+        self.ecn
+    }
+
+    /// 设置 ECN Counts
+    ///
+    /// # Arguments
+    /// `ecn_count` - ECN Counts
+    #[inline(always)]
+    pub(crate) fn set_ecn(&mut self, ecn_count: ECNCounts) {
+        self.ecn = Some(ecn_count)
+    }
+
+    /// 获取 ACK 范围
+    ///
+    /// # Returns
+    /// 返回 ACK 范围
+    #[inline(always)]
+    pub(crate) fn get_ranges(&self) -> &[ACKRange] {
+        &self.ranges
+    }
+
+    /// 设置 ACK 范围
+    ///
+    /// # Arguments
+    /// `ranges` - ACK 范围列表
+    #[inline(always)]
+    pub(crate) fn set_ranges(&mut self, ranges: &[ACKRange]) {
+        self.ranges.extend_from_slice(ranges)
     }
 }
 
@@ -111,11 +217,11 @@ impl Serializer for ACKFrame {
         payload_size += util::write_varint(self.largest, w)?;
         payload_size += util::write_varint(self.delay, w)?;
         payload_size += util::write_varint(self.ranges.len() as u64, w)?;
-        payload_size += util::write_varint(self.first_range, w)?;
+        payload_size += util::write_varint(self.first_range as u64, w)?;
 
         for range in self.ranges.iter() {
-            payload_size += util::write_varint(range.gap, w)?;
-            payload_size += util::write_varint(range.length, w)?;
+            payload_size += util::write_varint(range.gap as u64, w)?;
+            payload_size += util::write_varint(range.length as u64, w)?;
         }
 
         if let Some(ecn) = self.ecn {
@@ -145,7 +251,7 @@ impl Deserializer for ACKFrame {
         let range_count = range_count.value as usize - 1;
 
         let first_range = util::read_varint(r)?;
-        self.first_range = first_range.value;
+        self.first_range = first_range.value as usize;
         payload_size += first_range.size;
 
         for _ in 0..range_count {
@@ -156,8 +262,8 @@ impl Deserializer for ACKFrame {
             payload_size += length.size;
 
             self.ranges.push(ACKRange {
-                gap: gap.value,
-                length: length.value,
+                gap: gap.value as usize,
+                length: length.value as usize,
             });
         }
 
